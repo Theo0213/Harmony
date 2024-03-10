e5b2e9768a88fff82203e0a3f38fcd60b9f704a6
Theo_ <theolouise@hotmail.fr>
2024-03-10 20:51:40 +0100

    client secure / server websocket transports

--- pages/api/socket/io.ts
+++ pages/api/socket/io.ts
@@ -16,6 +16,7 @@ const ioHandler = (req: NextApiRequest, res: NextApiResponseServerIo) => {
     const io = new ServerIO(httpServer, {
       path: path,
       addTrailingSlash: false,
+      transports: ['websocket'],
     });
 
 
