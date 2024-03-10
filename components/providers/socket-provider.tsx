7bec4f7db33e58dd174387963fdb47fc1ba3cb8c
Theo_ <theolouise@hotmail.fr>
2024-03-10 19:09:01 +0100

    logs socket error

--- components/providers/socket-provider.tsx
+++ components/providers/socket-provider.tsx
@@ -41,6 +41,17 @@ export const SocketProvider = ({ children }: { children: React.ReactNode }) => {
       setIsConnected(false);
     });
 
+    socketInstance.on("connect_error", (err: any) => {
+      // the reason of the error, for example "xhr poll error"
+      console.log(err.message);
+    
+      // some additional description, for example the status code of the initial HTTP response
+      console.log(err.description);
+    
+      // some additional context, for example the XMLHttpRequest object
+      console.log(err.context);
+    });
+
     setSocket(socketInstance)
 
     return () => {
--- pages/api/socket/io.ts
+++ pages/api/socket/io.ts
@@ -19,6 +19,15 @@ const ioHandler = (req: NextApiRequest, res: NextApiResponseServerIo) => {
       pingTimeout: 60000,
       transports: ["websocket", "polling"]
     });
+
+    io.engine.on("connection_error", (err) => {
+      console.log(err.req);      // the request object
+      console.log(err.code);     // the error code, for example 1
+      console.log(err.message);  // the error message, for example "Session ID unknown"
+      console.log(err.context);  // some additional error context
+    });
+
+
     res.socket.server.io = io;
     res.end();
   }
